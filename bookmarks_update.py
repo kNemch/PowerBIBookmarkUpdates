09fe4586ad709164488f59de186197adda5d28f7
Kateryna Nemchenko <Kateryna_Nemchenko@epam.com>
2023-03-09 18:09:57 +0200

    script updates

--- bookmarks_update.py
+++ bookmarks_update.py
@@ -10,8 +10,8 @@ from zipfile import ZipFile
 
 
 """GLOBAL VARIABLES"""
-# These are the default path values. They may be replaced if the user enters custom directory path using CLI
 """Path constants"""
+# The values for these global path variables are assigned in the setup_worspace_paths() function, depending on the CLI user input
 WORKSPACE_PATH:str = None   # workspace directory with all the data
 SOURCE_PATH:str = None      # directory with original .pbix files
 TEMP_PATH:str = None        # directory for temporary files
@@ -47,16 +47,60 @@ def get_cli_args():
     parser.add_argument("-m", "--month", type=int, help="New value for Month", required=False)
     return parser.parse_args()
 
-def setup_worspace_paths():
-    WORKSPACE_PATH = os.getcwd()  # workspace directory with all the data
+
+def setup_worspace_paths(cli_workspace_path:str):
+    """Assigning the paths to the global variables"""
+    global WORKSPACE_PATH, SOURCE_PATH, TEMP_PATH, RESULTS_PATH
+    WORKSPACE_PATH = cli_workspace_path if cli_workspace_path else os.getcwd()  # workspace directory with all the data
     SOURCE_PATH = os.path.join(WORKSPACE_PATH, "source")  # directory with original .pbix files
     TEMP_PATH = os.path.join(WORKSPACE_PATH, "temp")      # directory for temporary files
     RESULTS_PATH = os.path.join(WORKSPACE_PATH, "result") # directory for the modified .pbix files
 
 
+def get_the_list_of_pbix_files():
+    for folderName, _, filenames in os.walk(WORKSPACE_PATH):
+        for filename in filenames:
+            if filename[-5:] == ".pbix" and not folderName.__contains__():
+                # create complete filepath of file in directory
+                temp_dir_file_path = os.path.join(folderName, filename)
+                # path for the archive hierarchy
+                archive_path = temp_dir_file_path[len(temp_dir_path)+1:]
+                # selecting the compression mode depending on archive file type
+                # writing the file to result archive
+
+                print(folderName, subfolders, filename, temp_dir_file_path, archive_path)
+
+
+def create_workspace_hierarchy():
+    """Checking if directories exist"""
+    if not os.path.exists(RESULTS_PATH):
+        os.mkdir(RESULTS_PATH)
+    if not os.path.exists(TEMP_PATH):
+        os.mkdir(TEMP_PATH)
+    if not os.path.exists(SOURCE_PATH):
+        os.mkdir(SOURCE_PATH)
+        print("Please, place all the .pbix files into the 'source' directory, and then rerun the script")
+    else:
+        print("The directories structure is OK")
+    
+
+
+
+
+
 if __name__ == "__main__":
     cli_args = get_cli_args()
+    print("Workspace Directory: {}".format(cli_args.directory))
+    print("New values for the reports: Year - {}, Month - {}".format(cli_args.year, cli_args.month))
+
+    setup_worspace_paths(cli_args.directory)
+    create_workspace_hierarchy()
+    
 
 
-    print("Directory: {}".format(cli_args.directory))
-    print("Year: {} Month: {}".format(cli_args.year, cli_args.month))
+
+    
+
+    print(WORKSPACE_PATH, SOURCE_PATH, TEMP_PATH, RESULTS_PATH, sep="\n")
+
+    
